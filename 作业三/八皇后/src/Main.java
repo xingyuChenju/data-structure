94031278e0c03ab58c125be1d7f0d692107cab56
xingyuChenju <22925152+xingyuChenju@users.noreply.github.com>
2018-10-05 14:36:37 +0800

    2222

--- 作业三/八皇后/src/Main.java
+++ 作业三/八皇后/src/Main.java
@@ -1,9 +1,34 @@
-public class Main {
-    public static void eightQueue(){
 
+public class Main {
+    public static int[] results = new int[8];
+    public static void eightQueue(int start,int level){
+        results[level] = start;
+        for (int i =0;i<results.length;i++){
+            if (isOk(i+1,level+1)){
+                results[level+1] =i+1;
+                if (level+2==results.length){
+                 for (int j = 0;j<results.length;j++){
+                     System.out.print(results[j]);
+                 }
+                 System.out.println();
+                }
+                 else{
+                        eightQueue(i+1, level + 1);
+                    }
+                }
+            }
+        }
+
+    public static boolean isOk(int position,int level){
+        for (int i =0;i<level;i++){
+            if (results[i]==position|Math.abs(position-results[i])==Math.abs(level-i)){
+                return false;
+            }
+        }
+        return true;
     }
     public static void main(String[] args){
-
+        eightQueue(1,0);
     }
 
 }
